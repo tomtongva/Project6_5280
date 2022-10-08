2d76898f9ab1bdabd7e77287f5614601fa1aa3d4
Tom Va <tomtongva@gmail.com>
2022-10-08 07:52:15 -0400

    initial

---
+++ index.js
@@ -0,0 +1,42 @@
+const twilio = require('twilio');
+const { MessagingResponse } = require('twilio').twiml; // respond to text message
+const express = require('express');
+const { TaskQueueRealTimeStatisticsPage } = require('twilio/lib/rest/taskrouter/v1/workspace/taskQueue/taskQueueRealTimeStatistics');
+
+const app = express();
+const port = 80;
+
+const accountSid = 'AC06e57af70a21df8ddbd50147d53dc6b0'; // Your Account SID from www.twilio.com/console
+const authToken = '7148510de1995ea197ebc2ef68fa06df'; // Your Auth Token from www.twilio.com/console
+
+const client = new twilio(accountSid, authToken);
+
+function testMsg() {
+    client.messages
+    .create({
+        body: 'Hello from Node',
+        to: '9809224519', // Text this number
+        from: '+19259400731', // From a valid Twilio number
+    })
+    .then((message) => console.log(message.sid));
+
+}
+
+app.post('/sms', (req, res) => { // respond to text message
+    const twiml = new MessagingResponse();
+  
+    twiml.message('The Robots are coming! Head for the hills!');
+  
+    res.type('text/xml').send(twiml.toString());
+});
+
+app.get('/', (req, res) => {
+    testMsg();
+
+    res.send("Hello world");
+});
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`Listening on port ${port}`);
+    console.log(`${process.env.PORT}`);
+  });
