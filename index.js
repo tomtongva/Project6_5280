2d0188aefb8d9e6e5e7aa12c1d415672f45a9be6
Tom Va <tomtongva@gmail.com>
2022-10-10 20:51:23 -0400

    always keep symptom 0

--- index.js
+++ index.js
@@ -236,7 +236,7 @@ app.post('/sms', async (req, res) => { // respond to text message
 
 function removeValueFromArray(array, value) {
     var indexOfValue = array.indexOf(value);
-    if (indexOfValue !== -1) {
+    if ((indexOfValue !== -1) && value !== "symptom None") {
         array.splice(indexOfValue, 1);
     }
 }
